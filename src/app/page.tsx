bda7b3448a2fde8c1ef5ae0435a0e2710e33e680
Alexandre Sampaio <alexandre.sampaio137@gmail.com>
2025-07-03 10:54:11 +0200

    feat: setup palette HSL + typographie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,23 @@ import { Title, Paragraph } from "@/components/typographie";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20  ">
        <main>
       <Title>Bienvenue sur mon portfolio</Title>
       <Paragraph>
         Voici un exemple de paragraphe qui utilise automatiquement la bonne
         police.
       </Paragraph>
-     
-     
+<div className="bg-primary text-primary-foreground p-4 rounded shadow">
+  ✔ Couleur `bg-primary` OK !
+</div>
+<div className="bg-background text-foreground p-4">
+  ✅ Test couleur `bg-background`
+</div>
+
     </main>
   
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center ">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.ts",
-    "./**/*.html"
+    "./src/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
-        fontFamily: {
-        title : ["var(--font-gowun-dodum)"],
-        body : ["var(--font-anuphan)"],
-  }
+      fontFamily: {
+        sans: ["var(--font-anuphan)", "sans-serif"],
+        body: ["var(--font-anuphan)", "sans-serif"],
+        title: ["var(--font-gowun-dodum)", "sans-serif"],
+      },
+      colors: {
+        background: { DEFAULT: "hsl(var(--background))" },
+        foreground: { DEFAULT: "hsl(var(--foreground))" },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        // ... autres couleurs
+      },
     },
   },
-  plugins: [],
-
-}
+  plugins: [require("tailwindcss-animate")],
+};
