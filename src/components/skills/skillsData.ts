bda347ae4cc68d49134bfdc85a1edb315ccf66e8
Alexandre Sampaio <alexandre.sampaio137@gmail.com>
2025-08-28 21:49:18 +0100

    feat: data for skills  created

---
+++ src/components/skills/skillsData.ts
@@ -0,0 +1,84 @@
+import { Skill } from '../../types/skills';
+
+export const skillsData: Skill[] = [
+  {
+    id: 1,
+    title: "JavaScript",
+    image: {
+      src: "/skills/javascript.png",
+      alt: "Logo JavaScript",
+    },
+  },
+  {
+    id: 2,
+    title: "TypeScript",
+    image: {
+      src: "/skills/typescript.png",
+      alt: "Logo TypeScript",
+    },
+  },
+  {
+    id: 3,
+    title: "React",
+    image: {
+      src: "/skills/react.png",
+      alt: "Logo React",
+    },
+  },
+  {
+    id: 4,
+    title: "Next.js",
+    image: {
+      src: "/skills/nextjs.png",
+      alt: "Logo Next.js",
+    },
+  },
+  {
+    id: 5,
+    title: "Node.js",
+    image: {
+      src: "/skills/nodejs.png",
+      alt: "Logo Node.js",
+    },
+  },
+  {
+    id: 6,
+    title: "Express.js",
+    image: {
+      src: "/skills/expressjs.png",
+      alt: "Logo Express.js",
+    },
+  },
+  {
+    id: 7,
+    title: "PostgreSQL",
+    image: {
+      src: "/skills/postgresql.png",
+      alt: "Logo PostgreSQL",
+    },
+  },
+  {
+    id: 8,
+    title: "MongoDB",
+    image: {
+      src: "/skills/mongodb.png",
+      alt: "Logo MongoDB",
+    },
+  },
+  {
+    id: 9,
+    title: "Git",
+    image: {
+      src: "/skills/git.png",
+      alt: "Logo Git",
+    },
+  },
+  {
+    id: 10,
+    title: "Docker",
+    image: {
+      src: "/skills/docker.png",
+      alt: "Logo Docker",
+    },
+  },
+];
