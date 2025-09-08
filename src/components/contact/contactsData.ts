227d26ea1d1eb8dd6b0dc156c2ffe6caf8e9c56a
Alexandre Sampaio <alexandre.sampaio137@gmail.com>
2025-09-08 19:24:26 +0200

    feat: contactData.ts

---
+++ src/components/contact/contactsData.ts
@@ -0,0 +1,28 @@
+import { Contact } from './../../types/contact';
+
+
+export const contactsData: Contact[] = [
+  {
+    id: 1,
+    type: "Email",
+    value: "",
+    href: "",
+    ariaLabel: "Envoyer un email",
+  },
+  {
+    id: 2,
+    type: "LinkedIn",
+    value: "Mon LinkedIn",
+    href: "https://www.linkedin.com/in/votre-profil",
+    ariaLabel: "Voir mon profil LinkedIn",
+  },
+  {
+    id: 3,
+    type: "GitHub",
+    value: "Mon GitHub",
+    href: "",
+    ariaLabel: "Voir mon profil GitHub",
+  },
+  ]
+
+  
